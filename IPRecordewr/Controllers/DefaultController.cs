61495e8ca100644ab83ce0635bc41b21c6c436ad
Vladimir Korukov <Vladimir Korukov>
2017-02-07 23:12:35 -0500

    Addind code

---
+++ IPRecordewr/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IPRecordewr.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public string Index(string who)
+        {
+            if (this.HttpContext.Application.AllKeys.Contains("ip" + who))
+                return (string)this.HttpContext.Application["ip" + who];
+            return "Not set";
+        }
+
+        public void Record(string who)
+        {
+            this.HttpContext.Application["ip" + who] = this.Request.UserHostAddress;
+        }
+    }
+}
---
+++ IPRecordewr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IPRecordewr.MvcApplication" Language="C#" %>
